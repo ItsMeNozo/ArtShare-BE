76c8f462990fe935b9f857ad62dd042ce5ed5351
Phan Trương Quý Hòa <93481453+quyhoaphantruong@users.noreply.github.com>
2025-03-08 16:58:20 +0700

    setup swagger

--- src/example/example.controller.ts
+++ src/example/example.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { ExampleService } from './example.service';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('example')
+@ApiTags('users')
 export class ExampleController {
   constructor(private exampleService: ExampleService) { }
   
   @Get()
+  @ApiOperation({ summary: 'Get example' })
+  @ApiBearerAuth()
+  @ApiResponse({ status: 200, description: 'Return all example.' })
   async findAll() {
     return await this.exampleService.findAll();
   }
