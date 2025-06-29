d9c6deb90a38a017807581e8091053b6657696b4
ItsMeNozo <dungtuyetngoc@gmail.com>
2025-06-29 17:08:57 +0700

    feat: remove backend  production URL from CORS configuration

--- src/notification/notification.gateway.ts
+++ src/notification/notification.gateway.ts
@@ -23,7 +23,6 @@ import { NotificationService } from './notification.service';
         adminUrl,
         // Add production URLs explicitly as fallback
         'https://artsharezone-black.vercel.app',
-        'https://artsharebe.id.vn',
       ];
       
       // Allow same-origin requests and specified origins
